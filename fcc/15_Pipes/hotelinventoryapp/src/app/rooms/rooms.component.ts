1c3b9696e597bc1bb1011e34b26af2562378f114
pranshubisht9 <pranshubisht9@gmail.com>
2023-06-01 16:00:46 +0530

    adding pipes

---
+++ fcc/15_Pipes/hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { Room, RoomList } from './rooms';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.css']
+})
+export class RoomsComponent implements OnInit {
+
+  noOfRooms = 12;
+
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5
+  }
+
+  roomList: RoomList[] = [
+    {
+      roomNumber:1,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wifi, TV, Bathroom, Kitchen',
+      price: 500,
+      photos: 'https://images.pexels.com/photos/20943/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=400',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021')
+
+    },
+
+    {
+      roomNumber:2,
+      roomType: 'Premium Room',
+      amenities: 'Air Conditioner, Free Wifi, TV, Bathroom, Kitchen',
+      price: 1500,
+      photos: 'https://images.pexels.com/photos/20943/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=400',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021')
+
+    },
+
+    {
+      roomNumber:3,
+      roomType: 'Pro Room',
+      amenities: 'Air Conditioner, Free Wifi, TV, Bathroom, Kitchen',
+      price: 1000,
+      photos: 'https://images.pexels.com/photos/20943/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=400',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021')
+
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
